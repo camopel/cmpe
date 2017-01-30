c2999aa0a681abee1943a8ebd02049b5a17cfe1b
camopel@gmail.com <camopel@gmail.com>
2017-01-30 01:33:32 -0800

    modified:   routes/upload.js

--- routes/upload.js
+++ routes/upload.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
 module.exports =  function(db){
-	function generateRandomPassword()
-	{
-		var dict = "0123456789abcdefghijklmnopqrstuvwxyz";
-		var pass="";
-		for(var i=0;i<6;i++)
-		{
-			pass += dict.charAt(Math.floor((Math.random() * 36)));
-		}
-		return pass;
-	}
+	// function generateRandomPassword()
+	// {
+		// var dict = "0123456789abcdefghijklmnopqrstuvwxyz";
+		// var pass="";
+		// for(var i=0;i<6;i++)
+		// {
+			// pass += dict.charAt(Math.floor((Math.random() * 36)));
+		// }
+		// return pass;
+	// }
 	function worksheetParse(dataType,rows)
 	{
 		var ws = [];
@@ -75,11 +75,8 @@ module.exports =  function(db){
 			res.redirect("/view/login.html");
 		}
 		else{
-			if (!req.files){
-				res.send({"success":"false","msg":"No file uploaded"});
-			}
-			else
-			{
+			if (!req.files){res.send({"success":"false","msg":"No file uploaded"});}
+			else{
 				var file = req.files["upload_file"];	
 				//console.log(file.mimetype); //application/vnd.openxmlformats-officedocument.spreadsheetml.sheet		
 				file.mv(req.app.locals.uploadDir+file.name, function(err){
@@ -89,7 +86,28 @@ module.exports =  function(db){
 						var workbook = XLSX.readFile(req.app.locals.uploadDir+file.name);
 						var first_sheet_name = workbook.SheetNames[0];
 						var worksheet = workbook.Sheets[first_sheet_name];
+						var A1 = worksheet['A1'];
+						var A2 = worksheet['B1'];
+						var A3 = worksheet['C1'];
+						var A4 = worksheet['D1'];
+						var A5 = worksheet['E1'];
+						var A6 = worksheet['F1'];
+						var A7 = worksheet['G1'];
+						if(req.body.upload_for=="Faculty" && (A1==null || A2==null || A3==null || A4==null || A1.v!='sjsuid' || A2.v!='lastname' && A3.v!='firstname' && A4.v!='email')){
+							res.send({"success":"false", "msg":"Header not match FACULTY format!"});
+							return;
+						}
+						else if(req.body.upload_for=="Conversion" && (A1==null || A2==null || A3==null || A4==null || A1.v!='sjsuid' || A2.v!='semester' && A3.v!='conversion' && A4.v!='comment')){
+							res.send({"success":"false", "msg":"Header not match CONVERSION format!"});
+							return;
+						}
+						else if(req.body.upload_for=="Project" && (A1==null || A2==null || A3==null || A4==null || A5==null || A6==null|| A7==null|| A1.v!='sjsuid' || A2.v!='semester' && A3.v!='program' && A4.v!='industry advisor'  || A5.v!='student'  || A6.v!='project title' || A7.v!='points')){
+							res.send({"success":"false", "msg":"Header not match PROJECT format!"});
+							return;
+						}
+						//console.log(worksheet);
 						var json = XLSX.utils.sheet_to_json(worksheet);
+						//console.log(json);
 						var map={
 							"Faculty":{datatype:"user",collection:"users"},
 							"Conversion":{datatype:"conv",collection:"conversions"},
